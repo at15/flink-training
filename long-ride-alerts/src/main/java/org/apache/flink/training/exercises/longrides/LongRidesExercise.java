75cde16719272aa904dd71fa48214c1ed015e534
Pinglei Guo <at15@outlook.com>
2022-01-21 17:46:29 -0800

    [lab4] Passed test but may leave endTime states behind
    If timer on startTime fires before endTime arrives, endTime
    will not find startTime in state (removed by timer) and save
    endTime state hoping startTime arrives later (and it never will ... )
    
    Haven't check the discussion on official solutions yet.

--- long-ride-alerts/src/main/java/org/apache/flink/training/exercises/longrides/LongRidesExercise.java
+++ long-ride-alerts/src/main/java/org/apache/flink/training/exercises/longrides/LongRidesExercise.java
@@ -21,7 +21,10 @@ package org.apache.flink.training.exercises.longrides;
 import org.apache.flink.annotation.VisibleForTesting;
 import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.state.MapState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.TimerService;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
@@ -30,10 +33,10 @@ import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.training.exercises.common.datatypes.TaxiRide;
 import org.apache.flink.training.exercises.common.sources.TaxiRideGenerator;
-import org.apache.flink.training.exercises.common.utils.MissingSolutionException;
 import org.apache.flink.util.Collector;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 /**
  * The "Long Ride Alerts" exercise.
@@ -47,7 +50,9 @@ public class LongRidesExercise {
     private final SourceFunction<TaxiRide> source;
     private final SinkFunction<Long> sink;
 
-    /** Creates a job using the source and sink provided. */
+    /**
+     * Creates a job using the source and sink provided.
+     */
     public LongRidesExercise(SourceFunction<TaxiRide> source, SinkFunction<Long> sink) {
         this.source = source;
         this.sink = sink;
@@ -95,20 +100,92 @@ public class LongRidesExercise {
         job.execute();
     }
 
+    // <Long, TaxiRide, Long>
+    // <KeyType, StreamItemType, OutputType>
     @VisibleForTesting
     public static class AlertFunction extends KeyedProcessFunction<Long, TaxiRide, Long> {
+        // key is RideId, value is Timestamp
+        private transient MapState<Long, Long> startTimes;
+        private transient MapState<Long, Long> endTimes;
 
         @Override
         public void open(Configuration config) throws Exception {
-            throw new MissingSolutionException();
+            MapStateDescriptor<Long, Long> startTimesDesc = new MapStateDescriptor<>("startTimes", Long.class, Long.class);
+            MapStateDescriptor<Long, Long> endTimesDesc = new MapStateDescriptor<>("endTimes", Long.class, Long.class);
+            startTimes = getRuntimeContext().getMapState(startTimesDesc);
+            endTimes = getRuntimeContext().getMapState(endTimesDesc);
         }
 
         @Override
         public void processElement(TaxiRide ride, Context context, Collector<Long> out)
-                throws Exception {}
+                throws Exception {
+            TimerService timerService = context.timerService();
+            if (ride.isStart) {
+                // START
+                long startTime = ride.getEventTimeMillis();
+
+                // Have we seen the END?
+                Long endTime = endTimes.get(ride.rideId);
+                if (endTime != null) {
+                    // Compare
+                    tryAlert(startTime, endTime, out, ride.rideId);
+                    // Clear State
+                    endTimes.remove(ride.rideId);
+                } else {
+                    // Save START
+                    startTimes.put(ride.rideId, startTime);
+                    long twoHoursLater = startTime + TWO_HOURS;
+                    timerService.registerEventTimeTimer(twoHoursLater);
+                }
+            } else {
+                // END
+                long endTime = ride.getEventTimeMillis();
+
+                // Have we seen the START?
+                Long startTime = startTimes.get(ride.rideId);
+                if (startTime != null) {
+                    // Compare
+                    tryAlert(startTime, endTime, out, ride.rideId);
+                    // Clear State
+                    startTimes.remove(ride.rideId);
+                    long twoHoursLater = startTime + TWO_HOURS;
+                    timerService.deleteEventTimeTimer(twoHoursLater); // NOTE: learned from ut, I need to remove the timer.
+                } else {
+                    // Save END
+                    endTimes.put(ride.rideId, endTime);
+                    // TODO: we still need timer, although START always arrives, its 2h timer will remove startTime
+                    // from state. If END arrives after that e.g. 2h+1s, no one will clear the state..
+                }
+            }
+        }
 
         @Override
         public void onTimer(long timestamp, OnTimerContext context, Collector<Long> out)
-                throws Exception {}
+                throws Exception {
+            // TODO: why I cannot pass more context when registering timer? have to use state?
+            long rideId = context.getCurrentKey();
+            Long startTime = startTimes.get(rideId);
+            if (startTime != null) {
+                // Alert because END did not arrive in time, otherwise startTime should be null
+                out.collect(rideId);
+                startTimes.remove(rideId);
+            }
+            // END arrived before timer fired.
+        }
+
+        private static final long TWO_HOURS = TimeUnit.HOURS.toMillis(2);
+
+        private void tryAlert(long start, long end, Collector<Long> out, long rideId) {
+            if (moreThan2Hour(start, end)) {
+                out.collect(rideId);
+            }
+        }
+
+        private boolean moreThan2Hour(long start, long end) {
+            if (start > end) {
+                throw new RuntimeException("start is later than end");
+            }
+            return (end - start) > TWO_HOURS;
+        }
     }
 }
